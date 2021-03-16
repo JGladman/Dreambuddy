17dced1f87f94dffa0447eb16a1381df656bb336
Jacob <jacobgladman32@gmail.com>
2021-03-16 13:53:34 -0400

    Created header and navbar

---
+++ components/Layout.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import { StyleSheet, Text, View } from "react-native";
+
+const Layout = ({ content }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Header />
+      </View>
+      <View style={styles.content}>{content}</View>
+      <View style={styles.navbar}>
+        <Navbar />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    width: "100%",
+    height: "100%",
+  },
+  header: {
+    flex: 1,
+  },
+  content: {
+    flex: 10,
+  },
+  navbar: {
+    flex: 1,
+  },
+});
+
+export default Layout;
